9b8501026a20bc30cbd787dd218c73ad7bfc4535
Egor251 <90013790+Egor251@users.noreply.github.com>
2024-05-02 13:57:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import time
 import config
+import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.errors import FloodWait
+import database
+from datetime import datetime
 
 api_hash = config.api_hash
 api_id = config.api_id
@@ -13,35 +14,62 @@ api_id = config.api_id
 
 client = Client(name="my_account", api_hash=api_hash, api_id=api_id)
 
-
 @client.on_message(filters.private)
-def echo(client_object, message: Message):
+async def echo(client_object, message: Message):
     #client_object.send_message(message.chat.id, f"вы сказали: {message.text}")
+    user_id = message.from_user.id
     text = message.text.lower()
     text_array = text.split(' ')
-    if 'понятно' in text or 'ожидать' in text_array:
-        pass
-    else:
-        pass
+    with database.Session(autoflush=False, bind=database.engine) as session:
+        print(database.Msg(user_id=user_id))
+        test = session.query(database.Msg).filter(database.Msg.user_id == user_id).first()
 
+        if 'понятно' in text_array or 'ожидать' in text_array:
+            test.status = 'finished'
+            test.status_updated_at = datetime.now()
+            session.commit()
+        else:
+            if test is None:
+                msg = database.Msg(user_id=user_id, created_at=datetime.strftime(datetime.now(), '%d/%m/%Y, %H:%M:%S'), status='alive', status_updated_at=datetime.strftime(datetime.now(), '%d/%m/%Y, %H:%M:%S'), current_step=1)
+                session.add(msg)
+                session.commit()
+            else:
+                pass
+    while True:
+        with database.Session(autoflush=False, bind=database.engine) as session:
+            #test = session.query(database.Msg).all()
 
-@client.on_message(filters.command("type", prefixes='!') & filters.me)
-def type(client_object, message: Message):
-    input_text = message.text.split("!type ", maxsplit=1)[1]
-    temp_text = input_text
-    edited_text = ""
-    typing_symbol = "⁂"
+            step1 = session.query(database.Msg).filter(database.Msg.current_step == 1, database.Msg.status == 'alive').all()
+            for tmp in step1:
+                time_difference = datetime.now() - datetime.strptime(tmp.status_updated_at, '%d/%m/%Y, %H:%M:%S')
+                minutes_difference = time_difference.total_seconds() / 60
+                if minutes_difference > 6:
+                    await client.send_message(tmp.user_id, config.text1)
+                    tmp.status_updated_at = datetime.strftime(datetime.now(), '%d/%m/%Y, %H:%M:%S')
+                    tmp.current_step = 2
+                    session.commit()
 
-    while edited_text != input_text:
-        try:
-            message.edit(edited_text + typing_symbol)
-            time.sleep(0.05)
-            edited_text = edited_text + temp_text[0]
-            temp_text = temp_text[1:]
-            message.edit(edited_text)
-            time.sleep(0.05)
-        except FloodWait:
-            print("Превышен лимит сообщений")
+            step2 = session.query(database.Msg).filter(database.Msg.current_step == 1, database.Msg.status == 'alive').all()
+            for tmp in step2:
+                time_difference = datetime.now() - datetime.strptime(tmp.status_updated_at, '%d/%m/%Y, %H:%M:%S')
+                minutes_difference = time_difference.total_seconds() / 60
+                if minutes_difference > 39:
+                    await client.send_message(tmp.user_id, config.text2)
+                    tmp.status_updated_at = datetime.strftime(datetime.now(), '%d/%m/%Y, %H:%M:%S')
+                    tmp.current_step = 3
+                    session.commit()
+
+            step3 = session.query(database.Msg).filter(database.Msg.current_step == 1, database.Msg.status == 'alive').all()
+            for tmp in step3:
+                time_difference = datetime.now() - datetime.strptime(tmp.status_updated_at, '%d/%m/%Y, %H:%M:%S')
+                minutes_difference = time_difference.total_seconds() / 60
+                if minutes_difference > 1560:
+                    await client.send_message(tmp.user_id, config.text3)
+                    tmp.status_updated_at = datetime.strftime(datetime.now(), '%d/%m/%Y, %H:%M:%S')
+                    tmp.statue = 'finished'
+                    session.commit()
+
+        await asyncio.sleep(5)
 
 
 client.run()
